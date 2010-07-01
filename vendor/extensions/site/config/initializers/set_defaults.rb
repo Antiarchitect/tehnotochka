91524e515d165bcb5cebd294e4dd310f912d0244
Andrey Voronkov <voronkovaa@gmail.com>
2010-07-01 21:48:20 +1000

    Production global and mail config

--- vendor/extensions/site/config/initializers/set_defaults.rb
+++ vendor/extensions/site/config/initializers/set_defaults.rb
@@ -3,4 +3,18 @@ if Spree::Config.instance
   # locale defaults
   Spree::Config.set(:default_locale => 'ru-RU')
   Spree::Config.set(:allow_locale_switching => false)
+
+  # production global config
+  Spree::Config.set(:site_name => 'Техноточка')
+  Spree::Config.set(:site_url => 'tehnotochka.heroku.com')
+
+  # production mail config
+  Spree::Config.set(:enable_mail_delivery => true)
+  Spree::Config.set(:mail_host => 'smtp.sendgrid.net')
+  Spree::Config.set(:mail_domain => ENV['SENDGRID_DOMAIN'])
+  Spree::Config.set(:smtp_username => ENV['SENDGRID_USERNAME'])
+  Spree::Config.set(:smtp_password => ENV['SENDGRID_PASSWORD'])
+  Spree::Config.set(:mails_from => 'tehnotochka.heroku.com')
+  Spree::Config.set(:order_from => 'orders@tehnotochka.heroku.com')
+  Spree::Config.set(:mail_auth_type => MAIL_AUTH[1])
 end
