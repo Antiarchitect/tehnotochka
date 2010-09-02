ca29e28379d6dd75627c723f77bbf5ec578d3133
Andrey Voronkov <voronkovaa@gmail.com>
2010-09-02 15:33:02 +1000

    donw

---
+++ vendor/extensions/main_page/app/controllers/main_page_controller.rb
@@ -0,0 +1,8 @@
+class MainPageController < Spree::BaseController
+  rescue_from ActionView::MissingTemplate, :with => :render_404
+  helper 'products'
+  def index
+    taxonomy = Taxonomy.find_by_name('Категории')
+    @taxons = taxonomy.root.children.compact
+  end
+end
---
+++ vendor/extensions/main_page/config/routes.rb
@@ -0,0 +1,7 @@
+# Put your extension routes here.
+
+# map.namespace :admin do |admin|
+#   admin.resources :whatever
+# end
+
+map.root :controller => 'main_page', :action => 'index'
