068cc56231a47705c237d82b7bca8dc69a50e6ee
Andrey Voronkov <voronkovaa@gmail.com>
2010-09-10 01:58:52 +1000

    looks cool

--- vendor/extensions/main_page/config/initializers/main_page.rb
+++ vendor/extensions/main_page/config/initializers/main_page.rb
@@ -1,1 +1,3 @@
-Spree::Config.set(:stylesheets => Spree::Config[:stylesheets] + ',main_page')
+unless Spree::Config[:stylesheets].include?('main_page')
+  Spree::Config.set(:stylesheets => "#{Spree::Config[:stylesheets]},main_page")
+end
