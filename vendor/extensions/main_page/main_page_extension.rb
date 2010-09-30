0ed2cab2ce201ca4a2afae01e0e1d8543ed8250f
Andrey Voronkov <voronkovaa@gmail.com>
2010-09-30 19:32:00 +1000

    meta tags optimization, removed empty migration

--- vendor/extensions/main_page/main_page_extension.rb
+++ vendor/extensions/main_page/main_page_extension.rb
@@ -13,10 +13,27 @@ class MainPageExtension < Spree::Extension
   # end
   
   def activate
-
-    # make your helper avaliable in all views
-    # Spree::BaseController.class_eval do
-    #   helper YourHelper
-    # end
+    Spree::BaseHelper.module_eval do
+      def meta_data_tags
+        taxon_names = Taxon.all(:select => [:name], :conditions => ['taxons.parent_id IS NOT NULL']).map { |t| t.name }.join(' ')
+        "".tap do |tags|
+          if self.respond_to?(:object) && object
+            if object.respond_to?(:meta_keywords) and object.meta_keywords.present?
+              tags << tag('meta', :name => 'keywords', :content => [taxon_names, object.meta_keywords].join(' ')) + "\n"
+            else
+              tags << tag('meta', :name => 'keywords', :content => taxon_names) + "\n"
+            end
+            if object.respond_to?(:meta_description) and object.meta_description.present?
+              tags << tag('meta', :name => 'description', :content => [taxon_names, object.meta_description].join(' ')) + "\n"
+            else
+              tags << tag('meta', :name => 'description', :content => taxon_names) + "\n"
+            end
+          else
+            tags << tag('meta', :name => 'keywords', :content => taxon_names) + "\n"
+            tags << tag('meta', :name => 'description', :content => taxon_names) + "\n"
+          end
+        end
+      end
+    end
   end
 end
